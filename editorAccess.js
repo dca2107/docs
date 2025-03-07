e929ad36a60042c1d9f767c53dd6b99487d583f2
fasta <146788352+dca2107@users.noreply.github.com>
2025-03-07 10:42:50 -0600

    Update editorAccess.js

--- editorAccess.js
+++ editorAccess.js
@@ -10,15 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
   if (loginForm) {
     loginForm.addEventListener('submit', function(event) {
       event.preventDefault();
-
       const username = document.getElementById('username').value;
       const password = document.getElementById('password').value;
-
       if (username === validUsername && password === validPassword) {
         alert('Acceso concedido. Bienvenido, editor.');
         document.getElementById('loginModal').style.display = 'none';
         document.getElementById('editorSection').style.display = 'block';
-        document.getElementById('redactorMenu').style.display = 'block';
+        document.getElementById('redactorMenu').style.display = 'flex';
         loginForm.reset();
       } else {
         alert('Credenciales incorrectas. Inténtalo nuevamente.');
@@ -26,12 +24,12 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  // Mostrar modal de login al hacer clic en el botón semi escondido
+  // Mostrar modal al hacer clic en el botón semi escondido
   document.getElementById('redactor-tools-btn').addEventListener('click', function() {
     document.getElementById('loginModal').style.display = 'block';
   });
 
-  // Cerrar el modal al hacer clic fuera del contenido
+  // Cerrar modal al hacer clic fuera de él
   window.addEventListener('click', function(event) {
     if (event.target === document.getElementById('loginModal')) {
       document.getElementById('loginModal').style.display = 'none';
